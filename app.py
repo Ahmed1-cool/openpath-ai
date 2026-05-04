eeee4e5ad4b25e3f04b9d29dbba51ba2eb795c1e
Ahmed1-cool <01228398131.ahmed@gmail.com>
2026-05-04 16:30:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def plan():
     deadline = request.args.get("deadline")
     style = request.args.get("style")
 
-    api_key = os.getenv("AIzaSyBYdvbc6t9BkNgAMJEWPoxOtgQHtBLPyOo")
+    api_key = os.getenv(AIzaSyBYdvbc6t9BkNgAMJEWPoxOtgQHtBLPyOo)
 
     prompt = f"""
 Create a structured daily study plan.
